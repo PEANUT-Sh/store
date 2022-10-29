98793a419aa1aa7bf97411454b45f65603ce0808
PEANUT-Sh <107972715+PEANUT-Sh@users.noreply.github.com>
2022-10-29 13:35:14 +0900

    Update install-pi-store.py

--- src/scripts/install-pi-store.py
+++ src/scripts/install-pi-store.py
@@ -5,4 +5,4 @@ import subprocess as lcmd
 question = msg.askyesno('確認-pi-store', 'Pi-storeをインストールしますか?')
 #反応の分岐
 if (question == True):
-    lcmd.run(['wget', '-qO-', 'https://raw.githubusercontent.com/PEANUT-Sh/store/main/src/scripts/install-pi-store.sh', '|', 'bash'])
+    lcmd.run("wget -qO- https://raw.githubusercontent.com/PEANUT-Sh/store/main/src/scripts/install-pi-store.sh | bash", shell=True)
