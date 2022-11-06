7a63e491c95c103a6e333c9f1575fbbd46d4282a
PEANUT-Sh <107972715+PEANUT-Sh@users.noreply.github.com>
2022-11-06 09:29:35 +0900

    v0.0.1

--- Python/main.py
+++ Python/main.py
@@ -3,10 +3,18 @@ base = tk.Tk()
 base.title("Pi-store home")
 base.configure(bg="white")
 
+icon = tk.PhotoImage(file="Python/images/ICON.png")
+
+canvas = tk.Canvas(bg="white", width=699, height=410)
+canvas.place(x=0, y=0)
+canvas.create_image(0, 0, image=icon, anchor=tk.NW)
+
 game = tk.Button(base, text='ゲーム', background='white', cursor='hand2')
 programming = tk.Button(base, text='プログラミング', background='white', cursor='hand2')
-game.pack()
-programming.pack()
+accessory = tk.Button(base, text='アクセサリ', background='white', cursor='hand2')
+game.grid()
+programming.grid()
+accessory.grid()
 
 
 base.mainloop()
