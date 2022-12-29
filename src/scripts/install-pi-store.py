1b383bb1dbed907769185531b935c6acf5e768ea
PEANUT-Sh <107972715+PEANUT-Sh@users.noreply.github.com>
2022-12-29 18:33:03 +0900

    wtf

--- src/scripts/install-pi-store.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import tkinter.messagebox as msg
-import subprocess as lcmd
-
-question = msg.askyesno('確認-pi-store', 'Pi-storeをインストールしますか?')
-#反応の分岐
-if (question == True):
-    lcmd.run("wget -qO- https://raw.githubusercontent.com/PEANUT-Sh/store/main/src/scripts/install-pi-store.sh | bash", shell=True)
--- src/scripts/install-pi-store.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-us=$(logname)
-cd /home/$us
-(
-    echo "#リポジトリの複製"
-    echo "リポジトリの複製"
-    #git clone https://github.com/PEANUT-Sh/store.git
-) | zenity --progress --title="あ" --text="あ"
